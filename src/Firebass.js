bf4aa9e08a10ad091bf4922147d8ef016eb79657
Nargiza Beishekenova <nargiza@MacBook-Air-Nargiza.local>
2022-02-10 19:31:47 +0600

    first commit

---
+++ src/Firebass.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI6DkiPejBS2YUvtusw5CXOdAGEe8Myow",
+  authDomain: "hackathon-rolexonline-store-js.firebaseapp.com",
+  projectId: "hackathon-rolexonline-store-js",
+  storageBucket: "hackathon-rolexonline-store-js.appspot.com",
+  messagingSenderId: "1023073153257",
+  appId: "1:1023073153257:web:e30ae9a02f9d555e264078"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
