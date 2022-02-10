cd8deb4e9e62eb510456f31fc36a85c5c2c8f7c1
Nargiza Beishekenova <nargiza@MacBook-Air-Nargiza.local>
2022-02-10 19:40:00 +0600

    hello

---
+++ src/Components/MyRoutes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyRoutes = () => {
+    return (
+        <div>
+            hello routes
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+export default MyRoutes;
